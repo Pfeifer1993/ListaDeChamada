3eab3bd95a1086300098f45d25329b524c411c02
Pfeifer1993 <80863899+Pfeifer1993@users.noreply.github.com>
2024-11-29 20:35:35 -0300

    Update aluno_visao.py

--- Dev/lista de chamada Douglinhas/untitled/ListaDeChamada/visao/aluno_visao.py
+++ Dev/lista de chamada Douglinhas/untitled/ListaDeChamada/visao/aluno_visao.py
@@ -1,21 +1,14 @@
 class VisaoAluno:
-    """
-    Classe responsável pela interação com o usuário.
-    """
+    """Gerencia a interação com o usuário para exibição e entrada de dados."""
+
     @staticmethod
     def exibir_alunos(alunos):
-        """
-        Exibe a lista de alunos cadastrados.
-        :param alunos: Lista de alunos.
-        """
+        """Exibe a lista de alunos cadastrados."""
         print("Lista de chamada:")
         for indice, aluno in enumerate(alunos, start=1):
             print(f"{indice}. {aluno}")
 
     @staticmethod
     def solicitar_nome_aluno():
-        """
-        Solicita o nome do aluno ao usuário.
-        :return: Nome do aluno digitado ou 'sair' para finalizar.
-        """
+        """Solicita o nome de um aluno ou um comando do usuário."""
         return input("Digite o nome do aluno (ou 'sair' para finalizar): ")
